e2fd46a4c8e823025eee9ace8b53de87d40b4473
Alexander <alexander.botello@g.austincc.edu>
2018-12-11 00:49:26 -0600

    Run Linter on Client codebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={exampleComponent} />
-          <Route path="/signin" exact component={exampleComponent} />
-          <Route path="/signup" exact component={exampleComponent} />
-          <Route path="/trip" exact component={exampleComponent} />
-          <Route path="/trip/:tripId" exact component={exampleComponent} />
-          <Route
-            path="/trip/:tripId/progress/:progressId"
-            exact
-            component={exampleComponent}
-          />
-          <Route path="/trip/create" exact component={exampleComponent} />
-          <Route path="/billing" exact component={exampleComponent} />
-          <Route path="/settings" exact component={exampleComponent} />
-        </Switch>
-      </Router>
-    );
-  }
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={exampleComponent} />
+        <Route path="/signin" exact component={exampleComponent} />
+        <Route path="/signup" exact component={exampleComponent} />
+        <Route path="/trip" exact component={exampleComponent} />
+        <Route path="/trip/:tripId" exact component={exampleComponent} />
+        <Route
+          path="/trip/:tripId/progress/:progressId"
+          exact
+          component={exampleComponent}
+        />
+        <Route path="/trip/create" exact component={exampleComponent} />
+        <Route path="/billing" exact component={exampleComponent} />
+        <Route path="/settings" exact component={exampleComponent} />
+      </Switch>
+    </Router>
+  )
 }
 
-const exampleComponent = () => <span>example component</span>;
-export default App;
+const exampleComponent = () => <span>example component</span>
+export default App
