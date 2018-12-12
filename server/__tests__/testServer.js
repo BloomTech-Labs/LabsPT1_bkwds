23539a614cf216d80699c558c853a1989011201b
Alexander <alexander.botello@g.austincc.edu>
2018-12-12 00:48:08 -0600

    Fix undefined error in

--- server/__tests__/testServer.js
+++ server/__tests__/testServer.js
@@ -2,8 +2,11 @@ import request from "supertest"
 import app from "../src/server"
 
 describe("Test server root path", () => {
-  test("It should start and run without error", async () => {
-    const response = await request(app).get("/")
-    expect(response.statusCode).toBe(200)
+  test("It should start and run without error", () => {
+    request(app)
+      .get("/")
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+      })
   })
 })
