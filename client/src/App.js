17623eec48f2c7821587077f5b9313cb370118a0
iamthuypham <tpham045@hotmail.com>
2018-12-12 15:59:03 -0800

    Add a high-level layout
    `LayoutWrapper` provides some or all of future components (i.e: sidebar, breadcrumb, authentication button) to all routes
    
    With `LayoutWrapper`, other devs don't have to add each mentioned component in each route

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,37 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import LayoutWrapper from './LayoutWrapper';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={exampleComponent} />
-        <Route path="/signin" exact component={exampleComponent} />
-        <Route path="/signup" exact component={exampleComponent} />
-        <Route path="/trip" exact component={exampleComponent} />
-        <Route path="/trip/:tripId" exact component={exampleComponent} />
-        <Route
-          path="/trip/:tripId/progress/:progressId"
-          exact
-          component={exampleComponent}
-        />
-        <Route path="/trip/create" exact component={exampleComponent} />
-        <Route path="/billing" exact component={exampleComponent} />
-        <Route path="/settings" exact component={exampleComponent} />
+        <LayoutWrapper>
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" exact component={Authentication} />
+          <Route path="/signup" exact component={Authentication} />
+          <Route path="/trip" exact component={TripsView} />
+          <Route path="/trip/:tripId" exact component={TripView} />
+          <Route
+            path="/trip/:tripId/progress/:progressId"
+            exact
+            component={Progress}
+          />
+          <Route path="/trip/create" exact component={TripCreate} />
+          <Route path="/billing" exact component={Billing} />
+          <Route path="/settings" exact component={Settings} />
+        </LayoutWrapper>
       </Switch>
     </Router>
   )
 }
 
-const exampleComponent = () => <span>example component</span>
+const Home = () => <div>Home component here!</div>
+const Authentication = () => <div>Authentication component here!</div>
+const TripsView = () => <div>Trips View component here!</div>
+const TripView = () => <div>Single Trip View component here!</div>
+const Progress = () => <div>Track and view trip progress here!</div>
+const TripCreate = () => <div>Create New Trip here!</div>
+const Billing = () => <div>Billing component here!</div>
+const Settings = () => <div>Settings component here!</div>
 export default App
