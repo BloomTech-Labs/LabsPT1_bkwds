2b8c6688473776d128bd3d8722416c2b13b992ab
JohnCoronel <john.m.coronel@gmail.com>
2018-12-12 20:56:11 -0800

    imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-
+import Login from "./login"
+import SignUp from "./signUp"
 const App = () => {
   return (
     <Router>
