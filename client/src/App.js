e599c644e92fb818079946f05db54da9db0f4341
iamthuypham <tpham045@hotmail.com>
2018-12-12 17:10:32 -0800

    Fix lint prettier error

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import LayoutWrapper from './LayoutWrapper';
+import LayoutWrapper from "./LayoutWrapper"
 
 const App = () => {
   return (
--- client/src/LayoutWrapper.js
+++ client/src/LayoutWrapper.js
@@ -1,33 +1,33 @@
 import React from "react"
-import { Container, Row, Col, ButtonGroup, Button } from 'reactstrap'
+import { Container, Row, Col, ButtonGroup, Button } from "reactstrap"
 
-export default class LayoutWrapper extends React.Component {
-    render() {
-        return (
-            <Container fluid>
-                <Row id="topMenu" xs="12">
-                    <Col id="breadcrumb" xs="auto" className="mr-auto">{this.props.location.pathname !== '/' && "Breadcumb here!"}</Col>
-                    <Col id="authButton" xs="auto">
-                        <Button>Sign In</Button>
-                        <Button>Sign Out</Button>
-                        <Button>Sign Up</Button>
-                    </Col>
-                </Row>
-                <Row>
-                    {this.props.location.pathname !== '/' &&
-                        <Col id="sidebar" xs="4">
-                            <ButtonGroup vertical>
-                                <Button>Trips</Button>
-                                <Button>Billing</Button>
-                                <Button>Settings</Button>
-                            </ButtonGroup>
-                        </Col>
-                    }
-                    <Col id="mainSection" xs="8">
-                        {this.props.children}
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+const LayoutWrapper = () => {
+  return (
+    <Container fluid>
+      <Row id="topMenu" xs="12">
+        <Col id="breadcrumb" xs="auto" className="mr-auto">
+          {this.props.location.pathname !== "/" && "Breadcumb here!"}
+        </Col>
+        <Col id="authButton" xs="auto">
+          <Button>Sign In</Button>
+          <Button>Sign Out</Button>
+          <Button>Sign Up</Button>
+        </Col>
+      </Row>
+      <Row>
+        {this.props.location.pathname !== "/" && (
+          <Col id="sidebar" xs="4">
+            <ButtonGroup vertical>
+              <Button>Trips</Button>
+              <Button>Billing</Button>
+              <Button>Settings</Button>
+            </ButtonGroup>
+          </Col>
+        )}
+        <Col id="mainSection" xs="8">
+          {this.props.children}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
