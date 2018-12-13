06858c66b3859c0738a8e1586a915dfda1803019
Alexander <alexander.botello@g.austincc.edu>
2018-12-13 15:53:28 -0600

    print error

--- server/src/db.js
+++ server/src/db.js
@@ -18,6 +18,7 @@ export const connect = () => {
       console.log("MONGO DB CONNECTED")
     })
     .catch(err => {
+      console.log(err)
       console.log(`Connection failed with config ${config.db.url}`)
     })
 }
