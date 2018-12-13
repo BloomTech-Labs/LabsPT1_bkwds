257494111f23e2575de62ace042b29195a1807b4
Alexander <alexander.botello@g.austincc.edu>
2018-12-13 12:31:14 -0600

    Kill database connection after tests

--- server/tests/testServer.js
+++ server/tests/testServer.js
@@ -1,7 +1,12 @@
 import request from "supertest"
 import app from "../src/server"
+import mongoose from "mongoose"
 
 describe("Test server root path", () => {
+  afterAll(done => {
+    mongoose.disconnect(done)
+  })
+
   test("It should start and run without error", async () => {
     const response = await request(app).get("/")
     expect(response.statusCode).toBe(200)
