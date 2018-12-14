ff62fe129aa2131d96015326ccace910fd3908cb
Alexander <alexander.botello@g.austincc.edu>
2018-12-14 15:16:03 -0600

    Add /register and /login endpoints to restRouter

--- server/src/api/restRouter.js
+++ server/src/api/restRouter.js
@@ -1,6 +1,9 @@
 import express from "express"
 import { userRouter } from "./resources/user"
+import { protect, register, login } from "./auth"
 
 export const restRouter = express.Router()
 
-restRouter.use("/users", userRouter)
+restRouter.use("/register", register)
+restRouter.use("/login", login)
+restRouter.use("/users", protect, userRouter)
