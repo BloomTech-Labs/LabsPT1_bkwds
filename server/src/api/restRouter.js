465cdf342a5ed6cd5759e3e5aed4e1846ba0d7a1
Alexander <alexander.botello@g.austincc.edu>
2018-12-14 18:08:43 -0600

    Update import path for auth functions

--- server/src/api/restRouter.js
+++ server/src/api/restRouter.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { userRouter } from "./resources/user"
-import { protect, register, login } from "./auth"
+import { protect, register, login } from "./modules/auth"
 
 export const restRouter = express.Router()
 
