bc1a4e72200fa6a37d97240dd0a46cb371ec4093
Andrew Jarrett <ahrjarrett@gmail.com>
2018-12-19 21:41:02 -0600

    Move ConnectedRouter into src/index, add ThemeProvider as child

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Route, Switch, Redirect } from "react-router-dom"
-import { ConnectedRouter } from "connected-react-router"
 import { connect } from "react-redux"
 
 import { history } from "../../store"
@@ -17,48 +16,44 @@ export const Billing = () => <div>Billing component here!</div>
 export const Settings = () => <div>Settings component here!</div>
 
 const App = props => (
-  <ConnectedRouter history={history}>
-    <Container>
-      {/* REMOVE THIS COMPONENT BEFORE PUSHING TO PRODUCTION; FOR TESTING UI CHANGE FOR LOGIN ONLY */}
-      <Route path="/" component={FakeLogin} />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route
-          path="/login"
-          exact
-          render={() => (props.isLoggedIn ? <Redirect to="/" /> : <Login />)}
-        />
-        <Route
-          path="/signup"
-          exact
-          render={() =>
-            props.isSignedUp ? <Redirect to="/login" /> : <Register />
-          }
-        />
-        <Route path="/trips" exact component={TripsView} />
-        <Route path="/trips/:tripId" exact component={TripView} />
-        <Route
-          path="/trip/create"
-          exact
-          render={() =>
-            !props.isLoggedIn ? <Redirect to="/" /> : <TripCreate />
-          }
-        />
-        <Route
-          path="/billing"
-          exact
-          render={() => (!props.isLoggedIn ? <Redirect to="/" /> : <Billing />)}
-        />
-        <Route
-          path="/settings"
-          exact
-          render={() =>
-            !props.isLoggedIn ? <Redirect to="/" /> : <Settings />
-          }
-        />
-      </Switch>
-    </Container>
-  </ConnectedRouter>
+  <Container>
+    {/* REMOVE THIS COMPONENT BEFORE PUSHING TO PRODUCTION; FOR TESTING UI CHANGE FOR LOGIN ONLY */}
+    <Route path="/" component={FakeLogin} />
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route
+        path="/login"
+        exact
+        render={() => (props.isLoggedIn ? <Redirect to="/" /> : <Login />)}
+      />
+      <Route
+        path="/signup"
+        exact
+        render={() =>
+          props.isSignedUp ? <Redirect to="/login" /> : <Register />
+        }
+      />
+      <Route path="/trips" exact component={TripsView} />
+      <Route path="/trips/:tripId" exact component={TripView} />
+      <Route
+        path="/trip/create"
+        exact
+        render={() =>
+          !props.isLoggedIn ? <Redirect to="/" /> : <TripCreate />
+        }
+      />
+      <Route
+        path="/billing"
+        exact
+        render={() => (!props.isLoggedIn ? <Redirect to="/" /> : <Billing />)}
+      />
+      <Route
+        path="/settings"
+        exact
+        render={() => (!props.isLoggedIn ? <Redirect to="/" /> : <Settings />)}
+      />
+    </Switch>
+  </Container>
 )
 
 const mapStateToProps = state => ({
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,25 @@
 import React from "react"
 import { render } from "react-dom"
 import { Provider } from "react-redux"
-import { store } from "./store"
+import { ConnectedRouter } from "connected-react-router"
+import { ThemeProvider } from "styled-components"
 
-import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import App from "./components/App"
+import "./index.css"
 import * as serviceWorker from "./serviceWorker"
+
+import App from "./components/App"
+import { store } from "./store"
+import { theme } from "./theme/variables"
+import { history } from "./store"
 
 render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 )
