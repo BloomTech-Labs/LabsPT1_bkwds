cd0163eb694889995f958fafa72fd5aaf036f098
Alexander <alexander.botello@g.austincc.edu>
2018-12-19 23:01:13 -0600

    Add dotenv dependency to load env

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 import { restRouter } from "./api"
+import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 
+dotenv.config()
 const app = express()
 
 app.use(express.json())
