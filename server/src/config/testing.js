5766c6bef61494d459fe0e40cc7445202c418666
Alexander <alexander.botello@g.austincc.edu>
2018-12-19 22:48:54 -0600

    Update testing config jwt_secret

--- server/src/config/testing.js
+++ server/src/config/testing.js
@@ -1,7 +1,7 @@
 export const config = {
   port: process.env.PORT || 5000,
   secrets: {
-    JWT_SECRET: null
+    JWT_SECRET: process.env.JWT_SECRET
   },
   db: {
     url: "mongodb://127.0.0.1/backwoods"
