3f99942a69c932f00c29d632663f4859f3c0ab95
Andrew Jarrett <ahrjarrett@gmail.com>
2018-12-20 03:46:25 -0600

    Fix typos in trip resource, mount at /api/trips

--- server/src/api/restRouter.js
+++ server/src/api/restRouter.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { userRouter } from "./resources/user"
+import { tripRouter } from "./resources/trip"
 import { protect, register, login } from "./modules/auth"
 
 export const restRouter = express.Router()
 
 restRouter.route("/register").post(register)
 restRouter.route("/login").post(login)
-restRouter.use("/users", protect, userRouter)
+restRouter.use("/users", userRouter)
+restRouter.use("/trips", tripRouter)
