87386fde4e47aa8b4c91bb87976ff0311e9f272a
Andrew Jarrett <ahrjarrett@gmail.com>
2019-01-04 01:40:11 -0600

    add custom input, button & error message to customInputs.js

--- client/src/components/forms/customInputs.js
+++ client/src/components/forms/customInputs.js
@@ -1,23 +1,45 @@
 import React from "react"
-import { Input } from "../../styles/theme/styledComponents"
 
-export const ValidatedInput = props => {
-  const { input, meta, type, placeholder } = props
-  return (
-    <div>
-      {meta.touched && meta.error ? (
-        <span className="form-error">{meta.error}</span>
-      ) : null}
-      <Input type={type} placeholder={placeholder} {...input} />
-    </div>
-  )
-}
+import { Input, Button } from "../../styles/theme/styledComponents"
+import { ErrorMessage } from "formik"
 
-export const CustomInput = props => {
-  const { input, type, placeholder } = props
-  return (
-    <div>
-      <Input type={type} placeholder={placeholder} {...input} />
-    </div>
-  )
-}
+export const CustomError = ({ name }) => (
+  <ErrorMessage name={name}>
+    {errorMessage => <div className="error client-error">{errorMessage}</div>}
+  </ErrorMessage>
+)
+
+// Error Message needs to come first to
+// make it easier to select Input as next sibling
+export const CustomInputWithError = ({
+  values, // `values` is made available by Formik
+  name, // Formik uses `name` to associate an Input with ErrorMessage
+  type,
+  placeholder,
+  onChange,
+  onBlur
+}) => (
+  <div className="form-field">
+    <CustomError name={name} />
+    <Input
+      type={type}
+      name={name}
+      onChange={onChange}
+      onBlur={onBlur}
+      placeholder={placeholder}
+      value={`${values[name]}`}
+    />
+  </div>
+)
+
+export const CustomButtonWithError = ({
+  submitError,
+  isSubmitting = false
+}) => (
+  <div className="form-field">
+    <Button className="btn" type="submit" disabled={isSubmitting}>
+      Log in
+    </Button>
+    {submitError && <div className="error server-error">{submitError}</div>}
+  </div>
+)
