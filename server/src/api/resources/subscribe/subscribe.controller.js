e658d7c8bc6d446d7d5756ba1356045459c3f505
iamthuypham <tpham045@hotmail.com>
2019-01-08 19:43:27 -0800

    Update subscribe route

--- server/src/api/resources/subscribe/subscribe.controller.js
+++ server/src/api/resources/subscribe/subscribe.controller.js
@@ -1,36 +1,6 @@
 import * as userController from "../user/user.controller"
 
-export const getSubscription = async (req, res, stripe) => {
-  const { subscribeId } = req.params
-  const subscription = await stripe.subscriptions.retrieve(subscribeId)
-  if (subscription) {
-    return res.status(200).send(subscription)
-  } else {
-    return res.status(500).send("Stripe error: Cannot retrieve subscription.")
-  }
-}
-
-export const subscribeToFreePlan = async (req, res, stripe) => {
-  const { source } = req.body
-  const customer = await stripe.customers.create({ source: source.id })
-
-  if (customer) {
-    const updatedRequest = {
-      ...req,
-      body: {
-        subscribed: true,
-        subdate: Date.now(),
-        customerId: customerId,
-        subscribeId: result.subscribeId
-      }
-    }
-    return userController.updateUser(updatedRequest, res)
-  } else {
-    res.status(500).send({ error: "Stripe error: cannot create customer." })
-  }
-}
-
-export const subscribeToPaidPlan = async (req, res, stripe) => {
+export const subscribe = async (req, res, stripe) => {
   const { planId, source } = req.body
   const customer = await stripe.customers.create({ source: source.id })
 
@@ -45,7 +15,7 @@ export const subscribeToPaidPlan = async (req, res, stripe) => {
         ...req,
         body: {
           subscribed: true,
-          subdate: Date.now(),
+          subDate: Date.now(),
           customerId: customer.id,
           subscribeId: subscription.id
         }
@@ -67,8 +37,8 @@ export const cancel = async (req, res, stripe) => {
     const updatedRequest = {
       ...req,
       body: {
-        subscribed: true,
-        subdate: Date.now(),
+        subscribed: false,
+        subDate: Date.now(),
         customerId: "",
         subscribeId: ""
       }
--- server/src/api/resources/subscribe/subscribe.restRouter.js
+++ server/src/api/resources/subscribe/subscribe.restRouter.js
@@ -6,16 +6,8 @@ export const subscribeRouter = express.Router()
 const stripe = config.stripe.instance
 
 subscribeRouter
-  .route("/:subscribeId")
-  .get((req, res) => subscribeController.getSubscription(req, res, stripe))
-
-subscribeRouter
-  .route("/free/:id")
-  .post((req, res) => subscribeController.subscribeToFreePlan(req, res, stripe))
-
-subscribeRouter
-  .route("/paid/:id")
-  .post((req, res) => subscribeController.subscribeToPaidPlan(req, res, stripe))
+  .route("/:id")
+  .post((req, res) => subscribeController.subscribe(req, res, stripe))
 
 subscribeRouter
   .route("/cancel/:id")
