16fe847188d7637747b4674e3d8b4781c38f86f6
Andrew Jarrett <ahrjarrett@gmail.com>
2019-01-12 14:52:33 -0600

    fix config to work with CRA REACT_APP_ convention

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,12 +1,12 @@
-let serverUri, stripeKey
+// GENERAL
+export const CLIENT_URI = process.env.REACT_APP_CLIENT_URI
+export const SERVER_URI = process.env.REACT_APP_SERVER_URI
 
-if (process.env.NODE_ENV === "production") {
-  serverUri = "https://backwoods-tracker.herokuapp.com/api"
-  stripeKey = process.env.STRIPE_KEY_CLIENT_PROD
-} else {
-  serverUri = "http://localhost:5000/api"
-  stripeKey = process.env.STRIPE_KEY_CLIENT_TEST
-}
+// STRIPE
+export const STRIPE_PLAN_ID_TEST =
+  process.env.REACT_APP_STRIPE_PLAN_ID_TEST || ""
+export const STRIPE_KEY = process.env.REACT_APP_STRIPE_KEY
+export const STRIPE_KEY_SERVER = process.env.REACT_APP_STRIPE_KEY_SERVER
 
-export const SERVER_URI = serverUri
-export const STRIPE_KEY = stripeKey
+// OAUTH
+export const FB_APP_ID = process.env.REACT_APP_FB_APP_ID
