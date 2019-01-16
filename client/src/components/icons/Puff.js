77c1abad70422828f71cc824223f861c7b807c74
ujaved931 <ujaved931@gmail.com>
2019-01-15 21:49:26 -0500

    fix

--- client/src/components/icons/Puff.js
+++ client/src/components/icons/Puff.js
@@ -10,7 +10,7 @@ const Puff = ({ height = "32px", width = "32px" }) => {
         xmlns="http://www.w3.org/2000/svg"
         stroke="#1E306E"
       >
-        <g fill="none" fill-rule="evenodd" stroke-width="2">
+        <g fill="none" fillRule="evenodd" strokeWidth="2">
           <circle cx="22" cy="22" r="1">
             <animate
               attributeName="r"
