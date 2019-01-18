301b188530accb3ced70bab27e4dc37382dd71c2
Alexander <alexander.botello@g.austincc.edu>
2019-01-17 22:07:52 -0600

    Trip list displays only logged in users trips

--- client/src/components/Trips.js
+++ client/src/components/Trips.js
@@ -2,36 +2,26 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 
 import TripCard from "./TripCard"
-import { getTrips } from "../redux/actions/trips"
-import { getTripsArray } from "../utils/selectors"
 import * as s from "../styles/TripCard.styles"
 import AddTripButton from "./AddTripButton"
 
 class Trips extends Component {
-  componentDidMount() {
-    this.props.getTrips()
-  }
-
   renderTrips() {
-    const { trips, loading } = this.props
+    const { trips } = this.props
 
     return (
       <div>
         <s.TripCardStyles>
-          {loading ? (
-            "Loading..."
-          ) : (
-            <div className="container">
-              {!trips.length && "No unarchived trips!"}
-              {trips.map(trip => (
-                <TripCard key={trip.id} trip={trip} archived={false} />
-              ))}
-              <AddTripButton
-                className="AddTripButton"
-                text={trips.length ? "Add New Trip" : "Add Your First Trip"}
-              />
-            </div>
-          )}
+          <div className="container">
+            {!trips.length && "No unarchived trips!"}
+            {trips.map(trip => (
+              <TripCard key={trip.id} trip={trip} archived={false} />
+            ))}
+            <AddTripButton
+              className="AddTripButton"
+              text={trips.length ? "Add New Trip" : "Add Your First Trip"}
+            />
+          </div>
         </s.TripCardStyles>
       </div>
     )
@@ -43,13 +33,7 @@ class Trips extends Component {
 }
 
 const mapStateToProps = state => ({
-  trips: getTripsArray(state),
-  loading: state.trips.loading
+  trips: state.auth.user.trips
 })
 
-const mapDispatchToProps = { getTrips }
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Trips)
+export default connect(mapStateToProps)(Trips)
