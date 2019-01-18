73478b5e4b865d3a49efe8593fd1e8bcf658b792
ujaved931 <ujaved931@gmail.com>
2019-01-18 03:47:33 -0500

    removed redundant imports

--- client/src/components/forms/SettingsForm.js
+++ client/src/components/forms/SettingsForm.js
@@ -45,7 +45,7 @@ const SettingsForm = ({
       handleSubmit,
       isSubmitting
     }) => (
-      <SettingsFormStyles className="responsive-design-settings">
+      <SettingsFormStyles>
         <div className="register-form custom-form">
           <Form onSubmit={handleSubmit}>
             <CustomInputWithError
