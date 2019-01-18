971c478936cf6f2615ebf886cb35efc1dafb505d
Alexander <alexander.botello@g.austincc.edu>
2019-01-17 18:31:42 -0600

    Add protected routes back in restRouter

--- server/src/api/restRouter.js
+++ server/src/api/restRouter.js
@@ -12,14 +12,9 @@ restRouter.route("/register").post(register)
 restRouter.route("/login").post(login)
 restRouter.route("/changePassword").post(changePassword)
 
-restRouter.use("/users", userRouter)
-restRouter.use("/trips", tripRouter)
-restRouter.use("/waypoints", waypointRouter)
-
-// restRouter.use("/users", protect, userRouter)
-// restRouter.use("/trips", protect, tripRouter)
-// restRouter.use("/waypoints", protect, waypointRouter)
-
+restRouter.use("/users", protect, userRouter)
+restRouter.use("/trips", protect, tripRouter)
+restRouter.use("/waypoints", protect, waypointRouter)
 restRouter.use("/subscribe", protect, subscribeRouter)
 
 restRouter.use("/reset_password", emailRouter)
