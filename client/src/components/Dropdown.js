d9601047fef0cc07f7729f3b4aa4b3fbd09e1d63
Alexander <alexander.botello@g.austincc.edu>
2019-01-19 16:38:09 -0600

    remove unused import

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
-import PropTypes from "prop-types"
 import {
   Dropdown,
   DropdownToggle,
