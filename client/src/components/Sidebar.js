f589e3fbfea810d4dcc341adc1a48c91cc0298db
Frank Faustino <nawzyah@gmail.com>
2019-01-19 23:35:44 -0800

    sidebar menu switches between Billing and Upgrade component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from "react"
+import { compose } from "redux"
+import { connect } from "react-redux"
 import { Link, withRouter } from "react-router-dom"
 import PropTypes from "prop-types"
+
 import { Button } from "../styles/theme/styledComponents"
 import * as s from "../styles/Sidebar.styles"
 
@@ -10,7 +13,7 @@ const SidebarLink = ({ to, displayName, pathname }) => (
   </Button>
 )
 
-const Sidebar = ({ location }) => {
+const Sidebar = ({ isSubscribed, location }) => {
   return (
     <s.SidebarStyles>
       <div className="sidebar-links">
@@ -32,14 +35,22 @@ const Sidebar = ({ location }) => {
         />
         <SidebarLink
           to="/app/trips/archived"
-          displayName="Archived Trips"
+          displayName="Archived trips"
           pathname={location.pathname}
         />
-        <SidebarLink
-          to="/app/billing"
-          displayName="Billing"
-          pathname={location.pathname}
-        />
+        {isSubscribed ? (
+          <SidebarLink
+            to="/app/billing"
+            displayName="Billing"
+            pathname={location.pathname}
+          />
+        ) : (
+          <SidebarLink
+            to="/app/upgrade"
+            displayName="Upgrade to Premium"
+            pathname={location.pathname}
+          />
+        )}
         <SidebarLink
           to="/app/settings"
           displayName="Settings"
@@ -81,4 +92,7 @@ SidebarLink.propTypes = {
   to: PropTypes.string.isRequired
 }
 
-export default withRouter(Sidebar)
+export default compose(
+  withRouter,
+  connect(({ auth }) => ({ isSubscribed: auth.user.subscribed }))
+)(Sidebar)
