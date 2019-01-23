2a486682c6127b2da20cd3c87fad6e9f40f15021
Andrew Jarrett <ahrjarrett@gmail.com>
2019-01-23 11:58:18 -0600

    modal now updates user and conditionally renders :tada:

--- client/src/components/DashboardHome.js
+++ client/src/components/DashboardHome.js
@@ -1,70 +1,148 @@
-import React from "react"
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import moment from "moment"
 
 import * as s from "../styles/Dashboard.styles"
-import Modal from "./modals/Modal"
+import Modal from "./Modals/Modal"
+import Autocomplete from "./Maps/Autocomplete"
 import { Form, GhostInput, Button } from "../styles/theme/styledComponents"
 import { UserPropTypes } from "./propTypes"
+import { openModal, closeModal } from "../redux/actions/modal"
+import { updateUserWithMsg } from "../redux/actions/settings"
 
-const DashboardHome = ({ user }) => {
-  return (
-    <s.DashboardStyles>
-      {user.displayName && <h1>Hi, {user.displayName}!</h1>}
-      {!user.displayName && <h1>Welcome {user.email}!</h1>}
-      <div>Last login: {moment(user.lastLogin).format("LLL")}</div>
-      <div>Login count: {user.loginCount}</div>
-      {user.formattedAddress && <div>Location: {user.formattedAddress}</div>}
+class DashboardHome extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formattedAddress: null,
+      displayName: "",
+      location: null,
+      query: ""
+    }
+    this.inputRef = React.createRef()
+  }
 
-      <Modal isOpen={modalIsOpen}>
-        {() => (
-          <div className="onboarding-flow">
-            <div className="flow-header">
-              <h4>Create your profile</h4>
+  componentDidMount() {
+    const { user } = this.props
+    if ((user && user.loginCount === 0) || !user.formattedAddress) {
+      this.setState({ displayName: user.displayName || "" })
+      this.props.openModal()
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.user.formattedAddress && !prevProps.user.formattedAddress) {
+      this.props.closeModal()
+    }
+  }
+
+  handleChange = key => e =>
+    this.setState({
+      [key]: e.target.value
+    })
+
+  updateUserValues = values => e => {
+    e.preventDefault()
+    const { user } = this.props
+    const { displayName } = this.state
+    const { formattedAddress, location } = values
+    const update = {
+      displayName,
+      coordinates: [location.lat, location.lng],
+      formattedAddress
+    }
+
+    this.props.updateUserWithMsg(user.id, update, "User update successful!")
+    this.setState({ location, formattedAddress })
+  }
+
+  render() {
+    const { user, modalIsOpen } = this.props
+    return (
+      <s.DashboardStyles>
+        {user.displayName && <h1>Hi, {user.displayName}!</h1>}
+        {!user.displayName && <h1>Welcome {user.email}!</h1>}
+        <div>Last login: {moment(user.lastLogin).format("LLL")}</div>
+        <div>Login count: {user.loginCount}</div>
+        {user.formattedAddress && <div>Location: {user.formattedAddress}</div>}
+
+        <Autocomplete
+          google={window.google}
+          inputRef={this.inputRef}
+          map={null}
+        >
+          {({ location, viewport, formattedAddress }) => {
+            console.log("RENDER ARGS:", location, viewport, formattedAddress)
+            return (
               <div>
-                This will give you a place to store trips and plan new ones
-                safely.
+                <Modal isOpen={modalIsOpen}>
+                  {() => (
+                    <div className="onboarding-flow">
+                      <div className="flow-header">
+                        <h4>Create your profile</h4>
+                        <div>
+                          This will give you a place to store trips and plan new
+                          ones safely.
+                        </div>
+                      </div>
+                      <Form
+                        onSubmit={this.updateUserValues({
+                          location,
+                          formattedAddress
+                        })}
+                      >
+                        <label>Display Name</label>
+                        <GhostInput
+                          value={this.state.displayName}
+                          onChange={this.handleChange("displayName")}
+                          placeholder="What should we call you?"
+                        />
+                        <label>Location</label>
+                        <GhostInput
+                          value={this.state.query}
+                          onChange={this.handleChange("query")}
+                          placeholder="Search for location..."
+                          ref={this.inputRef}
+                        />
+                        <div>{formattedAddress}</div>
+                        <div>{location.lat && location.lat + location.lng}</div>
+                        <div className="text-align-right">
+                          <Button className="btn">Save</Button>
+                        </div>
+                      </Form>
+                    </div>
+                  )}
+                </Modal>
               </div>
-            </div>
-            <Form
-              onSubmit={this.updateUserValues({
-                location,
-                formattedAddress
-              })}
-            >
-              <label>Display Name</label>
-              <GhostInput
-                value={this.state.displayName}
-                onChange={this.handleChange("displayName")}
-                placeholder="What should we call you?"
-              />
-              <label>Location</label>
-              <GhostInput
-                value={this.state.query}
-                onChange={this.handleChange("query")}
-                placeholder="Search for location..."
-                ref={this.inputRef}
-              />
-              <div>{formattedAddress}</div>
-              <div>{location.lat && location.lat + location.lng}</div>
-              <div className="text-align-right">
-                <Button className="btn">Save</Button>
-              </div>
-            </Form>
-          </div>
-        )}
-      </Modal>
-    </s.DashboardStyles>
-  )
+            )
+          }}
+        </Autocomplete>
+      </s.DashboardStyles>
+    )
+  }
 }
 
 DashboardHome.propTypes = {
-  user: PropTypes.objectOf(UserPropTypes)
+  user: PropTypes.objectOf(UserPropTypes),
+  modalIsOpen: PropTypes.bool.isRequired,
+  openModal: PropTypes.func.isRequired,
+  updateUserWithMsg: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-  user: state.auth.user
+  user: state.auth.user,
+  modalIsOpen: state.modal.isOpen
 })
 
-export default connect(mapStateToProps)(DashboardHome)
+const mapDispatchToProps = {
+  openModal,
+  closeModal,
+  updateUserWithMsg
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardHome)
--- client/src/redux/actions/settings.js
+++ client/src/redux/actions/settings.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import { SERVER_URI } from "../../config"
 import {
   INI_UPDATE_SETTINGS,
+  CLOSE_MODAL,
   UPDATE_SETTINGS_SUCCESS,
   UPDATE_SETTINGS_FAILURE,
   UPDATE_USER_IN_STORE
@@ -13,6 +14,27 @@ import { toast } from "react-toastify"
 const token = localStorage.getItem("token")
 if (token) {
   axios.defaults.headers.common["Authorization"] = token
+}
+
+export const updateUserWithMsg = (userId, values, msg) => dispatch => {
+  dispatch({ type: INI_UPDATE_SETTINGS })
+  axios
+    .put(`${SERVER_URI}/users/${userId}`, { ...values })
+    .then(res => {
+      const user = res.data
+      dispatch({ type: UPDATE_SETTINGS_SUCCESS })
+      dispatch({ type: UPDATE_USER_IN_STORE, payload: user })
+      toast.success(msg, {
+        position: toast.POSITION.BOTTOM_RIGHT
+      })
+      dispatch({ type: CLOSE_MODAL })
+    })
+    .catch(err => {
+      dispatch({ type: UPDATE_SETTINGS_FAILURE, payload: err })
+      toast.error(err.toString(), {
+        position: toast.POSITION.BOTTOM_RIGHT
+      })
+    })
 }
 
 export const updateEmail = (userId, email) => dispatch => {
