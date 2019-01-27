db3d9dae9dc4c9650754d040489b7afa02547dc9
Alexander <alexander.botello@g.austincc.edu>
2019-01-26 21:52:01 -0600

    Begin refactor of send_sms endpoint

--- server/src/api/modules/twilio.js
+++ server/src/api/modules/twilio.js
@@ -11,27 +11,41 @@ export const sendSMSAlert = async (req, res) => {
   // data params:
   //  - user id
   //  - trip id
-  //  - time specified by user
   const Twilio = twilio(sid, token)
   const data = await gatherResources(req.body)
+  if (!data.timeLimit) {
+    return res
+      .status(500)
+      .json("No time limit found. SMS alert will not be sent")
+  }
   const message = `Alert!\n${
     data.user
-  } has listed you as their emergency contact.\nIt's been 5 hours since ${
-    data.user
-  } last checked in.\nLast known coordinates: {${data.lat}, ${data.lon}}`
+  } has not completed their trip within their ${
+    data.timeLimit
+  } hour time limit.\nPlease attempt to contact contact them.\nLast known coordinates: ${
+    data.lat
+  }, ${data.lon}`
 
-  await Twilio.messages
-    .create({
-      body: message,
-      from: twilioNumber,
-      to: data.number
-    })
-    .then(() => {
-      res.status(200).json("SMS alert successfully sent")
-    })
-    .catch(() => {
-      res.status(500).json("Error sending message")
-    })
+  // PROBLEMS WITH THIS:
+  //  TESTS WILL HANG WHILE WAITING FOR SLEEP
+  //  IF TWILIO CALL FAILS CLIENT WILL NOT KNOW
+  //  BECAUSE WE CANNOT RES AGAIN AFTER HEADERS HAVE BEEN SENT
+  res.status(202).end("Safety alert timer started", async () => {
+    await sleep(data.timeLimit)
+    await Twilio.messages
+      .create({
+        body: message,
+        from: twilioNumber,
+        to: data.number
+      })
+      .then(() => {
+        return
+        console.log("SMS alert successfully sent")
+      })
+      .catch(() => {
+        console.log("Error sending message")
+      })
+  })
 }
 
 const gatherResources = async params => {
@@ -40,13 +54,23 @@ const gatherResources = async params => {
     path: "waypoints",
     model: "Waypoint"
   })
-  const location = findLastLocation(trip.waypoints)
+  let location = await findLastLocation(trip.waypoints)
+
+  if (!location) {
+    // If no waypoints are complete, then set location to be starting waypoint
+    let waypoint = trip.waypoints[0]
+    location = {
+      lat: waypoint.lat,
+      lon: waypoint.lon
+    }
+  }
   return {
     number: user.contact.number,
     name: user.contact.name,
     lat: location.lat,
     lon: location.lon,
-    user: user.displayName
+    user: user.displayName,
+    timeLimit: trip.timeLimit
   }
 }
 
@@ -56,4 +80,9 @@ const findLastLocation = waypoints => {
   })
   let lastLocation = filtered.length - 1
   return filtered[lastLocation]
+}
+
+const sleep = hours => {
+  let ms = hours * 3600000
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
