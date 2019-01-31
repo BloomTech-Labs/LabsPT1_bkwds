0a2a7e1550b6bf4fa2b42869a7fb305ae0d09ab9
iamthuypham <tpham045@hotmail.com>
2019-01-30 19:28:06 -0800

    Change to functional component

--- client/src/components/Maps/singleTripPublic/AppNav.js
+++ client/src/components/Maps/singleTripPublic/AppNav.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link } from "react-router-dom"
 import GitHubSvg from "../../icons/GitHubSvg"
 import * as s from "../../../styles/AppNav.styles"
 
-class AppNav extends Component {
-  render() {
-    return (
-      <div>
-        <s.NavStyles>
-          <div className="appnav-left">
-            <div className="logo">
-              <Link to="/">
-                bkwds<span className="logo-flourish">.</span>
-              </Link>
-            </div>
-          </div>
-          <div className="appnav-right">
-            <div className="navlinks-wrapper" />
-            <div className="call-to-action">
-              <GitHubSvg width="32px" height="32px" />
-            </div>
-          </div>
-        </s.NavStyles>
+const AppNav = () => (
+  <div>
+    <s.NavStyles>
+      <div className="appnav-left">
+        <div className="logo">
+          <Link to="/">
+            bkwds<span className="logo-flourish">.</span>
+          </Link>
+        </div>
       </div>
-    )
-  }
-}
+      <div className="appnav-right">
+        <div className="navlinks-wrapper" />
+        <div className="call-to-action">
+          <GitHubSvg width="32px" height="32px" />
+        </div>
+      </div>
+    </s.NavStyles>
+  </div>
+)
 
 export default AppNav
